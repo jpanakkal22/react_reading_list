7eeb68554cc5aa002c1623463583d3af5303a7e2
New Dad <josh_panakkal3122@hotmail.com>
2020-10-12 21:16:50 -0400

    udpate mongodb connection

--- server.js
+++ server.js
@@ -15,8 +15,16 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// Mongoose connection to database
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/reactreadinglist',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Start the API server
 app.listen(PORT, function() {
